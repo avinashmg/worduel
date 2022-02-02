6f2e60fd36348bbeb9c8d52da9da50bc4a71e1ae
Avinash M G <hatman290@gmail.com>
2022-02-02 22:35:23 +0530

    add multiplayer server

---
+++ index.js
@@ -0,0 +1,77 @@
+const WebSocketServer = require("ws").WebSocketServer;
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+//READ THE WORDLIST INTO AN ARRAY
+let words = [];
+let word = "";
+
+fs.readFile("words.txt", "utf8", function (err, data) {
+  words = data.split("\n");
+  console.log(words.length);
+  word = randomWord();
+  console.log(word);
+});
+
+function randomWord() {
+  let random = Math.floor(Math.random() * (words.length + 1));
+  return words[random].toUpperCase();
+}
+
+const wss = new WebSocketServer({ port: 8080 });
+const connections = {};
+const scoreboard = {};
+
+//Broadcast messages
+function broadcast(message) {
+  for (const key in connections) {
+    connections[key].send(JSON.stringify(message));
+  }
+}
+//End of broadcast messages
+wss.on("connection", function connection(ws) {
+  const id = uuidv4();
+  connections[id] = ws;
+  ws.send(
+    JSON.stringify({ type: "online", online: Object.keys(connections).length })
+  );
+  console.log("Connections: ", Object.keys(connections).length);
+  ws.on("close", function close() {
+    delete connections[id];
+    console.log("Connections: ", Object.keys(connections).length);
+  });
+
+  ws.on("message", function message(data) {
+    let r_word = JSON.parse(data).word;
+    let nickname = JSON.parse(data).nickname;
+    let line_score = 0;
+    let response = ["x", "x", "x", "x", "x"];
+    for (let i = 0; i < 5; i++) {
+      if (r_word[i] === word[i]) {
+        response[i] = "g";
+        line_score++;
+      } else {
+        if (word.indexOf(r_word[i]) !== -1) {
+          response[i] = "y";
+        }
+      }
+    }
+    if (line_score === 5) {
+      if (scoreboard[nickname]) {
+        scoreboard[nickname] = Object.keys(connections).length;
+      } else {
+        scoreboard[nickname] += Object.keys(connections).length;
+      }
+      ws.send(JSON.stringify({ type: "score", score: scoreboard[nickname] }));
+      broadcast({ type: "broadcast", winner: nickname, solution: word });
+      word = randomWord();
+    }
+    ws.send(JSON.stringify(response));
+    ws.send(
+      JSON.stringify({
+        type: "online",
+        online: Object.keys(connections).length,
+      })
+    );
+  });
+});
